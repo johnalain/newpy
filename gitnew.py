96d193792f7b0f54e2e270d5475f06a89c9826e3
john alain <johnalain572@gmail.com>
2024-01-07 09:31:23 +0200

    first

---
+++ gitnew.py
@@ -0,0 +1,13 @@
+#https://youtu.be/smKHKgedkP4 time: 2:03:00
+class Human:
+    name = input('enter ur name: ')
+    if name == 'michel':
+        print('welcome')
+        
+    else:
+        print('error')
+    
+
+
+ 
+
