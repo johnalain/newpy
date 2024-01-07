04f1212f6841497c87f15f3494580b36a2006702
john alain <johnalain572@gmail.com>
2024-01-07 09:54:17 +0200

    third

--- gitnew.py
+++ gitnew.py
@@ -1,18 +1,32 @@
 #https://youtu.be/smKHKgedkP4 time: 2:03:00
-class Human:
-    name = input('enter ur name: ')
-    if name == 'michel':
-        print('welcome')
+# class Human:
+#     name = input('enter ur name: ')
+#     if name == 'michel':
+#         print('welcome')
         
-    else:
-        print('error')
+#     else:
+#         print('error')
     
 # if u enter michel print welcome ir u print another name output error
 
-x = 3
-y = 7
-res = x+y
-print (res)
+# x = 3
+# y = 7
+# res = x+y
+# print (res)
+
+class Home:
+    def kitch(self):
+        print('for cooking and eating')
+    
+    def bedroom(self):
+        print('for sleeping and relaxing')
+        
+x = Home()
+x.kitch()
+x.bedroom()
+    
+
+        
 
  
 
