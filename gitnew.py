90332085283cce90a4d9cd55486c3bb0c1a37925
john alain <johnalain572@gmail.com>
2024-01-07 10:05:06 +0200

    fifth

--- gitnew.py
+++ gitnew.py
@@ -14,16 +14,32 @@
 # res = x+y
 # print (res)
 
+# class Home:
+#     #inside class functions must have self
+#     def kitch(self):
+#         print('for cooking and eating')
+    
+#     def bedroom(self):
+#         print('for sleeping and relaxing')
+        
+# x = Home()
+# x.kitch()
+# x.bedroom()
+
+# for cooking and eating
+# for sleeping and relaxing
+
 class Home:
+    #create function to let execution automatic
+    def __init__(self):
+        self.kitch()
+        self.bedroom()
     def kitch(self):
         print('for cooking and eating')
     
     def bedroom(self):
         print('for sleeping and relaxing')
-        
 x = Home()
-x.kitch()
-x.bedroom()
     
 
         
